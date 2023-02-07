283971480d9ed1238f72677ff98ed0a572c39b51
SonamSherpa122 <124115433+SonamSherpa122@users.noreply.github.com>
2023-02-06 22:54:18 -0500

    Add files via upload

---
+++ Assignment2Day6.java
@@ -0,0 +1,18 @@
+public class Assignment2Day6 {
+
+        public static void main(String[] args) {
+            System.out.println("Begin main");
+            int e= 3;
+            for(;e<=20;)
+
+            {
+                for(;e<=20;e++,e++)
+                {
+                    System.out.println("Your Odd Number is: " +e);
+                }
+                System.out.println("End main");
+            }
+        }
+    }
+
+
