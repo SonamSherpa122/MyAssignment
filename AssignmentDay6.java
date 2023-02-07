88a54a0ec5a95ce786dd6a6f3b7da1b0a27aa37a
SonamSherpa122 <124115433+SonamSherpa122@users.noreply.github.com>
2023-02-06 22:54:02 -0500

    Add files via upload

---
+++ AssignmentDay6.java
@@ -0,0 +1,15 @@
+public class AssignmentDay6 {
+    public static void main(String[] args) {
+
+        System.out.println("begin main");
+        int o = 4;
+        for (; o <= 30; ) {
+            {
+                for (; o <= 30; o++,o++)
+                    System.out.println("Your Even Number Started from: " + o);
+            }
+            System.out.println("End of Main");
+
+        }
+    }
+}
